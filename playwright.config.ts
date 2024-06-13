026994a2651188a2b3243292b47fe5c616506497
Jakub Rumpca <jakub.rumpca@gmail.com>
2024-06-13 17:20:40 +0200

    Add retries: 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ dotenv.config({ path: __dirname+'/.env' });
 
 export default defineConfig({
   testDir: './tests',
+  retries: 3,
   grep: testPlanFilter(),
   reporter: [["line"], ["allure-playwright"]],
   use: {
