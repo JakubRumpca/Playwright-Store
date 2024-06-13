1686ac1c024d863ed7f17387388ada70e7c535a1
Jakub Rumpca <jakub.rumpca@gmail.com>
2024-06-13 12:05:31 +0200

    Test case 05

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../page_object/loginPage';
 
-let loginPage: LoginPage
+let loginPage: LoginPage;
+const errorMessageLocator: string = '[data-test="error"]';
 
 test.beforeEach(async ({page}) => {
   loginPage = new LoginPage(page);
@@ -14,5 +15,10 @@ test('Should check if the standard user can log in', async ({ page }) => {
 
 test('Should check if the locked user can not log in', async ({ page }) => {
   await loginPage.login(process.env.LOCKED_USER, process.env.PASSWORD);
-  await expect(page.locator('[data-test="error"]')).toContainText('Epic sadface: Sorry, this user has been locked out.');
+  await expect(page.locator(errorMessageLocator)).toContainText('Epic sadface: Sorry, this user has been locked out.');
+});
+
+test('Should check if the standard user can not log in using the wrong password', async ({ page }) => {
+  await loginPage.login(process.env.STANDARD_USER, "wrongPassword");
+  await expect(page.locator(errorMessageLocator)).toContainText('Epic sadface: Username and password do not match any user in this service');
 });
